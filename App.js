18c981daf1f48494c54d573855a515b043917713
HaronStanley <129502270+HaronStanley@users.noreply.github.com>
2024-05-06 23:45:43 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+import Form from "./Form";
+
+const App = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
+
+// App.js
+/*import React from "react";
+import "./App.css";
+import Form from "./components/Form"; // Update the file path
+
+const App = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};*/
+
+/*export default App;
+import React from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+const App= () => {
+  return (
+  <div>
+    <Form />
+  </div>
+  ); 
+};
+
+export default App;*/
---
+++ Form.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Registerform from "./Registerform";
+
+
+const Form = () => {
+  return ( 
+  <div>
+    <Registerform />
+  </div>
+  );
+};
+
+export default Form;
+
+
+/*import React from "react";
+
+const Form = () => {
+  return <div>Form</div>;
+};
+
+export default Form;*/
